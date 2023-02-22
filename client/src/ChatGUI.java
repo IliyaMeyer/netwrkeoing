3481e4d2c50bd476ea86929d0db7fe3125730d02
NotNoshy <21053588+NotNoshy@users.noreply.github.com>
2023-02-22 18:33:07 +0200

    GUI updates

--- client/src/ChatGUI.java
+++ client/src/ChatGUI.java
@@ -1,5 +1,6 @@
 
 import java.awt.Color;
+import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -15,7 +16,8 @@ import javax.swing.text.StyledDocument;
 public class ChatGUI extends javax.swing.JFrame {
 
     String username;
-    String recipiantUsername;
+    String recipiant;
+    final LinkedList<String> connectedUsers = new LinkedList<String>();
     WhozuppClient client;
     final AtomicBoolean chatClosed = new AtomicBoolean(false);
 
@@ -37,26 +39,35 @@ public class ChatGUI extends javax.swing.JFrame {
         frame.setBounds(100, 100, 800, 600);
         username = JOptionPane.showInputDialog(frame, "Enter your username:", "Username", JOptionPane.PLAIN_MESSAGE);
         System.out.println("Username: " + username);
-        while (!client.sendNickname(username)) {
+        while (!client.sendNickname(username) && !username.matches("^[a-zA-Z0-9_]+$")) {
             System.out.println("Username invalid");
-            username = JOptionPane.showInputDialog(frame, "Username taken.\nEnter your newusername:", "Username", JOptionPane.PLAIN_MESSAGE);
+            username = JOptionPane.showInputDialog(frame, "Username invalid.\nEnter your new username:", "Username", JOptionPane.PLAIN_MESSAGE);
             System.out.println("Username: " + username);
         }
-        
+
+        setVisible(true);
         // TODO NOSHY
-        appendMessageToChat(username + " has conncted to WHOZUPP!", "Server");
+        appendMessageToChat(username + " has conncted to WHOZUPP!", null);
 
         usernameLabel.setText(username);
         System.out.println("Username set");
-
-        setVisible(true);
+        
         connectedStatusLabel.setText("Connected");
+
+        // get connected users and update GUI
+        // TODO get users
+        String[] options = new String[connectedUsers.size() + 1];
+        options[0] = "All users";
+        for (int i = 1; i < options.length; i++) {
+            options[i] = connectedUsers.get(i - 1);
+        }
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(options));
 
         // wait for messages
         new Thread(new Runnable() {
             @SuppressWarnings("empty-statement")
             public void run() {
-                WhozuppDataObject message;
+                WhozuppDataObject msgData;
 
                 if (chatClosed.get()) {
                     return;
@@ -64,13 +75,24 @@ public class ChatGUI extends javax.swing.JFrame {
 
                 // TODO NOSHYYYYYYYYYYYY MAKE PRETTY (LIKE YOU)
                 // IF type = 3 or 4 save the user list it changed.
-                while ((message = client.recieveMessage()) != null) {
-                    if (message.type == 1)
-                        appendMessageToChat(message.message, message.user);
-                    if (message.type == 3)
-                        appendMessageToChat(message.user + " has conncted to WHOZUPP!", "Server");
-                    if (message.type == 4)
-                        appendMessageToChat(message.user + " has disconncted from WHOZUPP!", "Server");
+                while ((msgData = client.recieveMessage()) != null) {
+                    if (msgData.type == 1) {
+                        appendMessageToChat(msgData.message, msgData.user);
+                    }
+                    if (msgData.type == 3) {
+                        synchronized (connectedUsers) {
+                            appendMessageToChat(msgData.user + " has conncted to WHOZUPP!", null);
+                            connectedUsers.add(msgData.user);
+                            updateComboBox();
+                        }
+                    }
+                    if (msgData.type == 4) {
+                        synchronized (connectedUsers) {
+                            appendMessageToChat(msgData.user + " has disconncted from WHOZUPP!", null);
+                            connectedUsers.remove(msgData.user);
+                            updateComboBox();
+                        }
+                    }
                 }
             }
         }).start();
@@ -87,7 +109,6 @@ public class ChatGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        recipiantField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         chatArea = new javax.swing.JTextPane();
@@ -95,8 +116,8 @@ public class ChatGUI extends javax.swing.JFrame {
         sendMessageButton = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         usernameLabel = new javax.swing.JLabel();
-        recipiantConfirmButton = new javax.swing.JButton();
         connectedStatusLabel = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -105,12 +126,9 @@ public class ChatGUI extends javax.swing.JFrame {
             }
         });
 
-        recipiantField.setEditable(false);
-        recipiantField.setText("User1");
-
         jLabel1.setText("Whozupp");
 
-        chatArea.setText("");
+        chatArea.setEditable(false);
         jScrollPane1.setViewportView(chatArea);
 
         sendMessageButton.setText("Send");
@@ -122,14 +140,14 @@ public class ChatGUI extends javax.swing.JFrame {
 
         jLabel2.setText("Chat To:");
 
-        recipiantConfirmButton.setText("Confirm");
-        recipiantConfirmButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                recipiantConfirmButtonActionPerformed(evt);
+        connectedStatusLabel.setText("Disconnected");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "All users" }));
+        jComboBox1.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                jComboBox1ItemStateChanged(evt);
             }
         });
-
-        connectedStatusLabel.setText("Disconnected");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -155,12 +173,10 @@ public class ChatGUI extends javax.swing.JFrame {
                                         .addComponent(messageArea)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(sendMessageButton))
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                                         .addComponent(jLabel2)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(recipiantField)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(recipiantConfirmButton))
+                                        .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(61, 61, 61))))))
         );
@@ -171,9 +187,8 @@ public class ChatGUI extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(recipiantField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
-                    .addComponent(recipiantConfirmButton))
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -212,7 +227,7 @@ public class ChatGUI extends javax.swing.JFrame {
 
     private void sendMessageButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendMessageButtonActionPerformed
         final String message = messageArea.getText();
-        final String recipiant = recipiantUsername;
+        final String recipiant = this.recipiant;
 
         final AtomicBoolean sent = new AtomicBoolean(false);
 
@@ -238,14 +253,15 @@ public class ChatGUI extends javax.swing.JFrame {
         appendMessageToChat(message, username);
     }//GEN-LAST:event_sendMessageButtonActionPerformed
 
-    private void recipiantConfirmButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_recipiantConfirmButtonActionPerformed
-        recipiantUsername = recipiantField.getText();
-    }//GEN-LAST:event_recipiantConfirmButtonActionPerformed
-
     private void chatClosedAction(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_chatClosedAction
         chatClosed.set(true);
         client.disconect();
     }//GEN-LAST:event_chatClosedAction
+
+    private void jComboBox1ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jComboBox1ItemStateChanged
+       recipiant = (String) jComboBox1.getSelectedItem();
+       chatArea.setText("");
+    }//GEN-LAST:event_jComboBox1ItemStateChanged
 
     private void appendMessageToChat(String message, String user) {
 
@@ -255,10 +271,16 @@ public class ChatGUI extends javax.swing.JFrame {
                 StyledDocument doc = chatArea.getStyledDocument();
                 Style style = doc.addStyle("Style", null);
                 StyleConstants.setForeground(style, Color.BLACK);
+                StyleConstants.setBold(style, true);
 
                 try {
-                    doc.insertString(doc.getLength(), user + ": ", style);
-                    doc.insertString(doc.getLength(), message + "\n", null);
+
+                    if (user != null) {
+                        doc.insertString(doc.getLength(), user + ": ", style);
+                        doc.insertString(doc.getLength(), message + "\n", null);
+                    } else {
+                        doc.insertString(doc.getLength(), message + "\n", style);
+                    }
                 } catch (BadLocationException ex) {
                     // should not occur
                     ex.printStackTrace();
@@ -266,6 +288,24 @@ public class ChatGUI extends javax.swing.JFrame {
             }
         });
 
+    }
+
+    private void updateComboBox() {
+
+        SwingUtilities.invokeLater(
+                new Runnable() {
+            public void run() {
+                synchronized (connectedUsers) {
+                    String[] options = new String[connectedUsers.size() + 1];
+                    options[0] = "All users";
+                    for (int i = 1; i < options.length; i++) {
+                        options[i] = connectedUsers.get(i - 1);
+                    }
+                    jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(options));
+                }
+            }
+
+        });
     }
 
     /**
@@ -306,13 +346,12 @@ public class ChatGUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextPane chatArea;
     private javax.swing.JLabel connectedStatusLabel;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField messageArea;
-    private javax.swing.JButton recipiantConfirmButton;
-    private javax.swing.JTextField recipiantField;
     private javax.swing.JButton sendMessageButton;
     private javax.swing.JLabel usernameLabel;
     // End of variables declaration//GEN-END:variables
